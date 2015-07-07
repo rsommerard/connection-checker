f2824930b687b5a0a3a372ccf9e1b16351d81a4b
Romain SOMMERARD <rsommerard@gmail.com>
2015-07-07 21:32:35 +0200

    Add script

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import urllib.request
+import time
+
+hostname = 'https://www.google.fr/'
+
+while True:
+    try:
+        response = urllib.request.urlopen(hostname)
+        print(time.strftime("[%H:%M:%S] Connection is back !"))
+        print('\a') # Make a bip system
+    except:
+        print(time.strftime("[%H:%M:%S] Connection lost."))
+        time.sleep(60)
